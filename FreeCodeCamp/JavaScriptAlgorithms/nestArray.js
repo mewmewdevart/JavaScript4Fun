ce3b2e7fdc712b08a9a4461a61f87333b4dd0332
Meowbutu <mewmewdevart@gmail.com>
2023-07-05 21:36:20 -0300

    scripts(content): up more exercises

---
+++ FreeCodeCamp/JavaScriptAlgorithms/nestArray.js
@@ -0,0 +1,12 @@
+/*
+Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+
+Create a nested array called myArray.
+*/
+
+
+const myArray = [["I love animals", 2],["Because",  2]];
